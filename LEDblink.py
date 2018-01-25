a52193f91c9e5d0aa067fc4545500cdc3ed3db2f
Lintang Wisesa <lintangwisesa@ymail.com>
2018-01-25 13:23:39 +0700

    Add files via upload

---
+++ LEDblink.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+led = 17
+GPIO.setup(led, GPIO.OUT)
+
+
+while True:
+    GPIO.output(led, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(led, GPIO.LOW)
+    sleep(1)
+
